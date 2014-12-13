ed6ccd32c1c7a0dedaa6448968f949e440ebce54
Karl Ward <karl@ghostghost.net>
2014-12-13 08:55:34 -0500

    layout fixes --KW

--- category.php
+++ category.php
@@ -16,7 +16,7 @@ get_header(); ?>
 
 			<?php if ( have_posts() ) : ?>
         <header class="archive-header">
-          <h1 class="entry-title"><?php printf( __( 'Category: %s', 'twentyfourteen' ), single_cat_title( '', false ) ); ?></h1>
+          <h1 class="entry-title"><?php printf( __( '%s', 'twentyfourteen' ), single_cat_title( '', false ) ); ?></h1>
 			  </header><!-- .archive-header -->
 			<?php
 					// Start the Loop.
