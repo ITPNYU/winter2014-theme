76230f6d0b6e8d7810fb29f109d7700f590b7d3d
Karl Ward <karl@ghostghost.net>
2014-12-15 15:22:36 -0500

    added front page template --KW

---
+++ front-page.php
@@ -0,0 +1,5 @@
+<?php
+// FIXME: this was a last minute thing, there should be a setting for 
+// redirecting to the category page
+wp_redirect('//itp.nyu.edu/shows/winter2014/category/projects/');
+?>
